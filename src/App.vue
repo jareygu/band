193c2236e9934a432f0d9301656fe8a1db4c0c16
gjl <296982075@qq.com>
2019-09-27 15:38:23 +0800

    滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,22 @@ export default {
   }
 }
 </script>
+<style>
+::-webkit-scrollbar {
+  width: 12px;
+}
+/* 滚动槽 */
+::-webkit-scrollbar-track {
+  box-shadow: inset006pxrgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background: rgba(0, 0, 0, 0.1);
+  box-shadow: inset006pxrgba(0, 0, 0, 0.5);
+}
+::-webkit-scrollbar-thumb:window-inactive {
+  background: rgba(159, 167, 164, 0.5);
+}
+</style>
